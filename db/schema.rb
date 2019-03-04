9189f01e3859894241ab944eebdb0c083d33d002
Jakob Merz <jakob.merz@whu.edu>
2019-03-04 17:26:47 +0100

    add bookings model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_161658) do
+ActiveRecord::Schema.define(version: 2019_03_04_162404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.integer "total_price"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "flats", force: :cascade do |t|
     t.integer "price_per_night"
