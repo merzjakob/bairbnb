9b2fabfbe8d9e1c5545a4728951ada3ee8911ff7
AntoineMassot <antoinemassot77@gmail.com>
2019-03-05 10:38:03 +0100

    add flats routes and flats controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'flats/index'
+  get 'flats/show'
+  get 'flats/new'
+  get 'flats/create'
+  get 'flats/destroy'
+  get 'flats/update'
   devise_for :users
   root to: 'pages#home'
+  resources :flats, only: [:index, :show, :new, :create, :destroy, :update]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
