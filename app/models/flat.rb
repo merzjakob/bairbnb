03b7c9dd643f9d9cd39504f336308b99477165f3
Jakob Merz <jakob.merz@whu.edu>
2019-03-05 16:48:03 +0100

    destroy booking when associated flat destroyed

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,6 +1,6 @@
 class Flat < ApplicationRecord
   belongs_to :user
   has_many :pictures
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
 end
