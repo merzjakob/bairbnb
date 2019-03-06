72a687b607ef45466e2dc40072a514de3c0ef75c
Jakob Merz <jakob.merz@whu.edu>
2019-03-06 14:28:13 +0100

    date input

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,7 +2,7 @@ class FlatsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    if params[:query][:city].present? && params[:query][:number_of_guests].present?
+    if params[:query][:city].present? || params[:query][:number_of_guests].present?
       @flats = Flat.where(city: params[:query][:city], number_of_guests: params[:query][:number_of_guests])
     else
       @flats = Flat.all
